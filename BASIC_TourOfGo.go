491d192ca83d03b5a67147a066533af91f674989
unknown <mentari.e.r@mail.ugm.ac.id>
2021-08-04 23:53:47 +0700

    BASIC_TourOfGo

--- BASIC_TourOfGo.go
+++ BASIC_TourOfGo.go
@@ -2,8 +2,15 @@ package fundamental_thinking_go
 
 import (
 	"fmt"
+	"math/cmplx"
 	// "math"
 	// "math/rand"
+)
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func add(x, y int) int {
@@ -21,7 +28,10 @@ func main() {
 	//-----------------------------------------------------------------
 
 	// [FUNCTION]
-	fmt.Println(add(42, 13))
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+	// fmt.Println(add(42, 13))
+	// a, b := swap("hello", "world")
+	// fmt.Println(a, b)
+
+	//[BASIC TYPES]
+	fmt.Printf("Type: Value: \n")
 }
